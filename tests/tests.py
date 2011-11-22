e91280e70697760c1e4bb756837746288f13d2fa
Oscar Celma <ocelma@ocelma.gracenote.gracenote.com>
2011-11-22 12:39:33 -0800

    updated U2 Achtung Baby IDs (new reluxe edition!) and added one more test for "Movies a Tracks" problem reported by @paul-obrien

--- tests/tests.py
+++ tests/tests.py
@@ -4,15 +4,15 @@ import itunes
 
 U2 = 'U2'
 U2_ONE = 'One'
-U2_ACHTUNGBABY = 'Achtung Baby'
+U2_ACHTUNGBABY = 'Achtung Baby (Deluxe Edition) [Remastered]' # 'Achtung Baby'
 
-U2_URL = 'http://itunes.apple.com/us/artist/u2/id78500?uo=4'
-U2_ACHTUNGBABY_URL = 'http://itunes.apple.com/us/album/achtung-baby/id368713?uo=4'
-U2_ONE_URL = 'http://itunes.apple.com/us/album/one/id368713?i=368617&uo=4'
-
-U2_ONE_ID = 368617
-U2_ACHTUNGBABY_ID = 368713
+U2_ONE_ID = 475391315 # Before it was 368617
+U2_ACHTUNGBABY_ID = 475390461 # Before it was 368713
 U2_ID = 78500
+
+U2_URL = 'http://itunes.apple.com/us/artist/u2/id%s?uo=4' % U2_ID
+U2_ACHTUNGBABY_URL = 'http://itunes.apple.com/us/album/achtung-baby-deluxe-edition/id%s?uo=4' % U2_ACHTUNGBABY_ID
+U2_ONE_URL = 'http://itunes.apple.com/us/album/one/id%s?i=%s&uo=4' % (U2_ACHTUNGBABY_ID, U2_ONE_ID)
 
 #SEARCHES
 def test_search_track():
@@ -72,7 +72,7 @@ def test_track_url():
 
 def test_album_length():
     item = itunes.lookup(U2_ACHTUNGBABY_ID)
-    assert_true(len(item.get_tracks()) == 12)
+    assert_true(len(item.get_tracks()) == 26) # 12)
 
 #TEXT: Unicode
 def test_unicode():
@@ -80,3 +80,9 @@ def test_unicode():
 
 def test_unicode2():
     assert_equal(itunes.search_artist('Björk')[:5], itunes.search_artist(u'Bj\xf6rk')[:5])
+
+def test_movie_as_track():
+    item = itunes.search(query='the godfather', media='movie')[0]
+    assert_equal(item.get_artist(), None)
+
+
